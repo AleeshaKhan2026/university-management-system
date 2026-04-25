7c18ce758a6430d21d38d83153ebf4fa71f097e4
AleeshaKhan2026 <aleeshakhan2028@gmail.com>
2026-04-25 18:09:19 +0500

    update API URL for production

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,7 +1,12 @@
 // API Configuration
-const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+// const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+//     ? 'http://localhost:5000/api'
+//     : 'https://your-backend-url.onrender.com/api';
+const API_BASE_URL =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://your-backend-url.onrender.com/api';
+    : 'https://university-management-system-production-2878.up.railway.app/api';
 
 
 // API Service Class
